5abfce1d0f54beb1f933a60a5354e488496661e5
Maru Song <m.song@fkf.mpg.de>
2025-10-22 09:00:53 +0200

    add comments for 20site Heisenberg example

--- examples/20site_Heisenberg_chain.py
+++ examples/20site_Heisenberg_chain.py
@@ -1,3 +1,8 @@
+"""
+Perform a GA simulation with the S-Ms mapping for a 20-site Heisenberg chain.
+`ref_csf` defines the S-Ms consistent CSF for `ref_ordering`.
+Therefore, the S-Ms mappling {site:coupling} is {1:1, 2:2, 3:1, 4:2, ..., 19:1, 20:2}.
+"""
 import GA_mod.run_GA as ga                                                              
 import GA_mod.crossover as co                                                           
 from GA_mod.measure_fitness import FitnessFunction                                      
