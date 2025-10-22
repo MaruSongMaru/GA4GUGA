b29ea349c648b2c3c1ff60bb782746c33fed68ca
Maru Song <m.song@fkf.mpg.de>
2025-10-22 10:58:49 +0200

    add a short description in the P-clsuter example

--- examples/P-cluster_48i40.py
+++ examples/P-cluster_48i40.py
@@ -1,3 +1,12 @@
+"""
+Perform a GA simulation for CAS(48,40) PN-cluster.
+The fitness function maximizes the gap between the highest and the lowest
+CSF energies over the 14 collinear CSFs (`collinear_csfs`).
+
+Since we restrict the permutation space to only permutations among the 8 Fe
+sites, `restricted_ordering_len` is set to 8, and `on_site_permutation`
+is set to (1,2,3,4,5) meaning there is no permutation within each Fe site.
+"""
 import GA_mod.run_GA as ga                                                              
 import GA_mod.crossover as co                                                           
 from GA_mod.measure_fitness import FitnessFunction                                      
