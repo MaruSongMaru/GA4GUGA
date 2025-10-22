760b64a89e2164664fdbf4eb0978077552a21ca2
Maru Song <m.song@fkf.mpg.de>
2025-10-22 10:47:29 +0200

    recover mistakenly removed parts for the fcidump -> Jmat feature

--- src/FCIDUMP_tools/xyz2heisenberg.py
+++ src/FCIDUMP_tools/xyz2heisenberg.py
@@ -126,9 +126,40 @@ def gen_heisenberg_fcidump(xyzfile: str, fcidumpname: str, nel: int, norb: int, 
         diag = J / 2.0
         integral_list.append([diag, j, j, i, i])
         integral_list.append([J, j, i, i, j])
-    return integral_list
+    dump_integrals(fcidumpname, nel, norb, ms, integral_list)
 
+#------------------------------------------------------------------------------#
+"""
+Collection of functions for writing an FCIDUMP file.
+This feature is redundant with a feature in IntegralClass.
+They have to be combined.
+"""
 
-if __name__ == '__main__':
-    gen_heisenberg_fcidump('4x4-square.xyz', 'FCIDUMP_4x4_square', 16, 16, 0,
-                           distance_range=(1.9, 2.1))
+def dump_integrals(fcidumpname, nel, norb, ms, integral_list):
+    "integral list has to be [[x1 i1 j1 k1 l1], [x2 i2 j2 k2 l2], ...]"
+
+    header = gen_header(nel, norb, ms)
+
+    with open(fcidumpname, 'w') as f:
+        f.write(header)
+        for ints in integral_list:
+            f.write(gen_int_manual(*ints))
+
+def gen_header(nel, norb, ms):
+    """
+    Generates the header for a manual fcidump file
+    Currently, it is assumed that there is no point group symmetry
+    """
+
+    header = f""" &FCI NORB=  {norb}, NELEC=  {nel}, MS2=  {ms},
+  ORBSYM= {', '.join(['1'] * norb)}
+  ISYM=0
+ &END\n"""
+
+    return header
+    
+def gen_int_manual(x, i, j, k, l, digits=12):
+    integral = f"""    {x:.{digits}f}    {i}    {j}    {k}    {l}\n"""
+    return integral
+
+#------------------------------------------------------------------------------#
