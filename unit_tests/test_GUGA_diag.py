9009dc5ca0b15fcdd2ffa1ce483fc3e1ec99eedb
Maru Song <m.song@fkf.mpg.de>
2025-10-24 00:15:28 +0200

    fix: update extrafile dir

--- unit_tests/test_GUGA_diag.py
+++ unit_tests/test_GUGA_diag.py
@@ -3,7 +3,7 @@ from FCIDUMP_tools.IntegralClass import *
 from GA_mod.GUGA_diag import *
 import numpy as np
 
-FILEPATH = "../../extra_files/"
+FILEPATH = "../extra_files/"
 
 diagelem_benchmark = np.load(FILEPATH + "Benzene_diagelem.npy")
 csfs = np.load(FILEPATH + "Singlet_6i6_CSFs.npy")
